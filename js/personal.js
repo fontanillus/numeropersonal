b8ba0bd5d7c64622347cc3257d464e05725dac0c
fontanillus <arcamapa@gmail.com>
2025-06-26 12:32:49 +0200

    Add files via upload

---
+++ js/personal.js
@@ -0,0 +1,13 @@
+     const significados = [
+                { numero: 1, descripcion: "Líder, independiente, pionero." },
+                { numero: 2, descripcion: "Cooperativo, diplomático, sensible." },
+                { numero: 3, descripcion: "Creativo, expresivo, sociable." },
+                { numero: 4, descripcion: "Organizado, trabajador, práctico." },
+                { numero: 5, descripcion: "Aventurero, libre, adaptable." },
+                { numero: 6, descripcion: "Responsable, protector, amoroso." },
+                { numero: 7, descripcion: "Introspectivo, espiritual, analítico." },
+                { numero: 8, descripcion: "Ambicioso, poderoso, materialista." },
+                { numero: 9, descripcion: "Humanitario, compasivo, idealista." },
+                { numero: 11, descripcion: "Visionario, iluminado, inspirador." },
+                { numero: 22, descripcion: "Constructor, maestro, realizador." }
+            ];
